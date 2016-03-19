5b69e49c1480ce96036757d7c3f29cdd0cfa3e1d
timirahj <timirah.james@gmail.com>
2016-03-19 16:44:10 -0700

    RealmSwift pod added

---
+++ Podfile
@@ -0,0 +1,9 @@
+use_frameworks!
+
+platform :ios, '8.0'
+
+target ‘Realm-API-Demo’ do
+
+pod ‘RealmSwift’
+
+end
---
+++ Pods/Target Support Files/Pods-Realm-API-Demo/Pods-Realm-API-Demo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Realm_API_DemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Realm_API_DemoVersionString[];
+
